6416b64e0e587f57a29eeef7db4220ee1ba8621e
TKG-gisi <19589407+TKG-gisi@users.noreply.github.com>
2026-03-15 17:26:02 +0900

    viewer update

--- static/app.js
+++ static/app.js
@@ -1,22 +1,25 @@
-// configuration
 
-const baseURL = "http://localhost:8000/"
 
-// main
-
+/* application configuration */
+const config = {
+    "viewerURL":"http://localhost:8000",
+    "apiURL":"https://golang.mipsparc.net"
+}
+// ==================================================================
 function index_addThreadOnMenu( threadInfo) {
     // prepare dom
     let template = document.querySelector("#threadItem");
     let clone = template.content.cloneNode(true);
     // insert data into template
     clone.querySelector(".thread").innerText = threadInfo.title;
-    clone.querySelector(".thread").href = baseURL + "thread/" + threadInfo.thread_id + ".html";
+
+    clone.querySelector(".thread").href = config.viewerURL + "/thread/" + threadInfo.thread_id + ".html";
     // insert datas finished here! ----------------
     let menu = document.getElementById("threadList");
     menu.appendChild(clone);
 }
 function showIndex (){
-    const request = new Request("top.json");
+    const request = new Request(config.apiURL);
     fetch(request)
     .then ( (response) => (response.json()))
     .then ( (data) => {
@@ -24,4 +27,43 @@ function showIndex (){
     })
 }
 
-window.addEventListener("load", (event) => {showIndex()})
+// ==================================================================
+
+
+
+
+// ==================================================================
+/* PATHと処理を紐付けるモジュール*/
+const pathBinder = {
+    bindingTable:[
+        // path : Javascript location pathname をLookupする想定。先頭の有効な文字は / である。
+        // func : 関数をバインドする
+        {"path": /^\/thread\/\d+$/, "func":"Thread Viewer"},
+        {"path": /^\/$/, "func": showIndex}
+    ],
+    // lookup : path -> function or none
+    lookup:function(pathKey) {
+        let filterResult = this.bindingTable.filter(
+            (bindItem) => {
+                return bindItem.path.test(pathKey)
+            }
+        )
+        if (filterResult.length == 0) {
+            // @todo fallback処理をここに書く 
+            filterResult.push = {"path": /^\/$/, "func": showIndex}
+
+        }
+        return filterResult[0].func;
+    }
+};
+// ==================================================================
+// VIEWER
+function viewer(){
+    let path = location.pathname;
+    console.log("current path:",path);
+    let hoge = pathBinder.lookup(path);
+    hoge();
+}
+// ---
+// application startup routine
+window.addEventListener("load", (event) => {viewer()})
